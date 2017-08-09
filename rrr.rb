8302adcc097ef9a72393e21f12cec058309683d5
Rob Vitaro <robvitaro@gmail.com>
2017-08-08 21:54:33 -0400

    add bundler and rack support

--- rrr.rb
+++ rrr.rb
@@ -1,15 +1,18 @@
 require 'sinatra'
+require 'sinatra/base'
 require 'Haml'
 require 'rss'
 require 'open-uri'
 
-get '/' do
-  @title = "rrr (rob's rss reader)"
-  url = 'http://www.tor.com/feed'
-  open(url) do |rss|
-    feed = RSS::Parser.parse(rss)
-    @feed = "Title: #{feed.channel.title}"
-    @items = feed.items
+class RRR < Sinatra::Base
+  get '/' do
+    @title = "rrr (rob's rss reader)"
+    url = 'http://www.tor.com/feed'
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      @feed = "Title: #{feed.channel.title}"
+      @items = feed.items
+    end
+    haml :index
   end
-  haml :index
 end
