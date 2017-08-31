25b625a12cd84ae4d4f8e257dfa51d309f81eadb
Rob Vitaro <robvitaro@gmail.com>
2017-08-30 23:13:02 -0400

    add mark as read feature; add delete action (not in UI yet)

--- rrr.rb
+++ rrr.rb
@@ -22,12 +22,21 @@ class RssLink
   property :created_at, DateTime
 end
 
+class RssItem
+  include DataMapper::Resource
+  property :id, Serial
+  property :guid, Text
+  property :read, Boolean
+end
+
+
 # Perform basic sanity checks and initialize all relationships
 # Call this when you've defined all your models
 DataMapper.finalize
 
 # automatically create the table
 RssLink.auto_upgrade!
+RssItem.auto_upgrade!
 
 class RRR < Sinatra::Base
 
@@ -44,7 +53,6 @@ class RRR < Sinatra::Base
   end
 
   post '/create' do
-    puts params[:feed_title]
     @post = RssLink.create(
         title: params[:feed_title],
         url: params[:feed_url],
@@ -59,15 +67,47 @@ class RRR < Sinatra::Base
     feed_info = rss_feed_info(url)
     @feed = feed_info.title
     @items = feed_info.items
+    add_rss_items(@items)
+    unless params[:all] == 'true'
+      @items.reject! { |item| !RssItem.first(guid: guid_extractor(item.guid), read: true).nil? }
+    end
     haml :feed
+  end
+
+  delete '/feed/:id' do
+    feed = RssLink.get(params[:id])
+    feed.destroy
+    redirect to('/')
+  end
+
+  get '/feed_item/mark_read' do
+    item = RssItem.first(guid: params[:guid])
+    puts item
+    item.read = true
+    item.save
+    redirect back
   end
 
   # Helpers
 
+  def add_rss_items(items)
+    items.each do |item|
+      if !RssItem.get(guid: item.guid)
+        RssItem.create(
+            guid: guid_extractor(item.guid),
+            read: false
+        )
+      end
+    end
+  end
+
+  def guid_extractor(guid)
+    guid.to_s[/\>(.*?)\<\//m, 1]
+  end
+
   def rss_feed_info(url)
     feed_info = nil
-    open(url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}) do |rss|
-      puts "the rss: #{rss}"
+    open(url, { ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE }) do |rss|
       feed = RSS::Parser.parse(rss)
       feed_info = FeedInfo.new(feed.channel.title, feed.items)
     end
--- tests/rrr_test.rb
+++ tests/rrr_test.rb
@@ -10,6 +10,7 @@ class RRRTest < Test::Unit::TestCase
 
   def setup
     RssLink.auto_migrate! # reset DB
+    RssItem.auto_migrate! # reset DB
   end
 
   def app
@@ -37,19 +38,41 @@ class RRRTest < Test::Unit::TestCase
     assert_equal 'The url', link.url
   end
 
-  def test_it_shows_feed
+  def test_it_shows_full_feed
     RssLink.create(
         title: 'The title',
         url: 'The url',
         created_at: Time.now
     )
-    items = [stub(:link => 'link_1', :title => 'title_1', :description => 'description_1'),
-             stub(:link => 'link_2', :title => 'title_2', :description => 'description_2'),]
+    items = [stub(:link => 'link_1', :title => 'title_1', :description => 'description_1', :guid => '<guid>abc</guid>'),
+             stub(:link => 'link_2', :title => 'title_2', :description => 'description_2', :guid => '<guid>efg</guid>')]
     feed_info = RRR::FeedInfo.new('The title', items)
     RRR.any_instance.stubs(:rss_feed_info).with('The url').returns(feed_info)
-    get '/feed/1'
+    RRR.any_instance.expects(:add_rss_items).with(items).returns(nil)
+    get '/feed/1?all=true'
     assert last_response.ok?
     assert last_response.body.include?('title_1')
+    assert last_response.body.include?('description_2')
+  end
+
+  def test_it_shows_feed_minus_read
+    RssLink.create(
+        title: 'The title',
+        url: 'The url',
+        created_at: Time.now
+    )
+    RssItem.create(
+        guid: 'abc',
+        read: true
+    )
+    items = [stub(:link => 'link_1', :title => 'title_1', :description => 'description_1', :guid => '<guid>abc</guid>'),
+             stub(:link => 'link_2', :title => 'title_2', :description => 'description_2', :guid => '<guid>efg</guid>')]
+    feed_info = RRR::FeedInfo.new('The title', items)
+    RRR.any_instance.stubs(:rss_feed_info).with('The url').returns(feed_info)
+    RRR.any_instance.expects(:add_rss_items).with(items).returns(nil)
+    get '/feed/1'
+    assert last_response.ok?
+    assert_false last_response.body.include?('title_1')
     assert last_response.body.include?('description_2')
   end
 
@@ -64,5 +87,35 @@ class RRRTest < Test::Unit::TestCase
     result = app.rss_feed_info('http://example.com')
     assert_equal expected, result
   end
+
+  def test_it_deletes_feed
+    RssLink.create(
+        title: 'The title',
+        url: 'The url',
+        created_at: Time.now
+    )
+    delete '/feed/1'
+    assert_equal 0, RssLink.all.length
+    link = RssLink.get(1)
+    assert_equal nil, link
+
+  end
+
+  def test_it_marks_feed_item_read
+    RssItem.create(
+        guid: 'abc',
+        read: false
+    )
+    get '/feed_item/mark_read?guid=abc'
+    assert_equal true, RssItem.first(guid: 'abc').read
+  end
+
+=begin
+  Planned features
+  - it can group feeds into categories
+  - it can have user accounts (log in/out)
+  - one page accordions for feeds, grid masonry for posts - ajaxed feeds
+=end
+
 
 end
